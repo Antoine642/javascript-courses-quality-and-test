777e3a083189b3a4bad015e8ea1561f65f527c57
Antoine Park <asuse35@hotmail.fr>
2024-12-16 14:14:46 +0100

    gh actions & readme

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,22 @@
 const { defineConfig } = require("@playwright/test");
 
 module.exports = defineConfig({
-  testDir: "./test/playwright", // Limite aux tests dans `playwright/`
+  testDir: "./test/playwright", // Dossier tests Playwright
   timeout: 30000,
   use: {
     baseURL: "http://localhost:3030",
-    headless: true, // Exécute les tests en mode headless par défaut
+    headless: true, // Exécute les tests en mode headless
     browserName: "chromium",
   },
-  reporter: [["html", { outputFolder: "playwright-report" }]], // Rapport HTML
+  reporter: [
+    ["html", { outputFolder: "playwright-report" }], // Rapport HTML généré
+    [
+      "@estruyf/github-actions-reporter",
+      {
+        title: "Résultats des tests Playwright",
+        useDetails: true,
+        showError: true,
+      },
+    ],
+  ],
 });
