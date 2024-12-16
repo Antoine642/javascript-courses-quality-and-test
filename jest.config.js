75645c06e6427482ba81c26f281cafd0c82070cf
Antoine Park <asuse35@hotmail.fr>
2024-12-16 14:45:00 +0100

    add jest coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,17 @@ module.exports = {
   testMatch: ["**/test/jest/**/*.test.js"],
   collectCoverage: true,
   coverageDirectory: "./coverage",
+  coverageReporters: [
+    "json-summary",
+    "text",
+    "lcov"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 };
